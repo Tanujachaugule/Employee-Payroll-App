d4f6d9bf7e3612a442ed7e94cccfbb17515d8e7f
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-04-06 20:29:01 +0530

    Added: Ability to save employee payroll to local storage

--- Payroll_form.js
+++ Payroll_form.js
@@ -1,36 +1,3 @@
-//D44UC2
-window.addEventListener('DOMContentLoaded', (event) => {
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
-        if(name.value.length == 0) {
-            textError.textContent = "";
-            return;
-        }
-        try {
-            (new EmployeePayrollData()).name = name.value;;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
-        }
-    });
-
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salRange.addEventListener('input', () => {
-        salary.innerHTML = salRange.value;
-    });
-});
-const save = () => {
-    try {
-        let employeePayrollData = createEmployeePayroll();
-        createAndUpdateStorage(employeePayrollData);
-    } catch (e) {
-        return;
-    }
-}
-
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
     try {
@@ -72,3 +39,86 @@ const getInputElementValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
 }
+
+
+const salary = document.querySelector('.salary-output');
+const salRange = document.querySelector('#salary');
+const username = document.querySelector('#name');
+const nameError = document.querySelector('#errormsg');
+const notes = document.querySelector('#notes');
+let departmentValues = [];
+let employeePayrollList = [];
+
+username.addEventListener('input', () => {
+  let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+  if (nameRegex.test(username.value)) {
+    username.style.border = '2px solid green';
+    nameError.style.visibility = 'hidden';
+  } else {
+    nameError.style.visibility = 'visible';
+    username.style.border = 'none';
+  }
+});
+
+//     const salary = document.querySelector('.salary-output');
+// const salRange = document.querySelector('#salary');
+
+salRange.addEventListener('input', () => {
+  salary.innerHTML = salRange.value;
+});
+
+
+function save() {
+    const profileImage = document.querySelector('input[name="profile"]:checked');
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const checkbox = document.querySelectorAll('input[class="checkbox"]:checked');
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    let startDate = day.value + '-' + month.value + '-' + year.value;
+    checkbox.forEach((dept) => {
+      departmentValues.push(dept.value);
+    });
+  
+    window.alert(
+      username.value +
+        ',' +
+        salRange.value +
+        ',' +
+        profileImage.value +
+        ',' +
+        gender.value +
+        ',' +
+        departmentValues +
+        ',' +
+        startDate +
+        ',' +
+        notes.value
+    );
+
+if (window.localStorage.key(1) !== null) {
+    employeePayrollList = JSON.parse(
+      window.localStorage.getItem('employeePayrollData')
+    );
+  }
+
+  let newEmployee = {
+    name: username.value,
+    profileImg: profileImage.value,
+    gender: gender.value,
+    department: departmentValues,
+    salary: salRange.value,
+    startDate: startDate,
+    notes: notes.value,
+  };
+
+  employeePayrollList.push(newEmployee);
+
+  window.alert(employeePayrollList);
+
+  window.localStorage.setItem(
+    'employeePayrollData',
+    JSON.stringify(employeePayrollList)
+  );
+
+  }
