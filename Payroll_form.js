a1a4fdd61b5268ad7f58ad9f69a56315f64c2268
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-04-07 19:41:50 +0530

    Added: Create header in home page of employee payroll page

--- Payroll_form.js
+++ Payroll_form.js
@@ -60,8 +60,7 @@ username.addEventListener('input', () => {
   }
 });
 
-//     const salary = document.querySelector('.salary-output');
-// const salRange = document.querySelector('#salary');
+
 
 salRange.addEventListener('input', () => {
   salary.innerHTML = salRange.value;
