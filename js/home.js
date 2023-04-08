fbe9540598d4bc408b6f623960a870b259fc6997
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-04-08 10:39:14 +0530

    Added: Used for loop to populate multiple employee payroll information

--- js/home.js
+++ js/home.js
@@ -6,23 +6,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {   
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
+        let innerHtml = `${headerHtml}`;
+        let empPayrollList = createEmployeePayrollJSON();
+        for (const empPayrollData of empPayrollList) {
+            innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" alt="" width="30" src="images/images.png"\>
             </td>
-                <td>Tanuja Chaugule</td>
-                <td>Female</td>
-                <td><div class='dept-label'>HR</div>
-                    <div class='dept-label'>Engineering</div></td>
-                <td>300000</td>
-                <td>1 Nov 2020</td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
                 <td>
-                    <img id="1" onclick="remove(this)" alt="delete"
+                    <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete"
                          src="images/Deleteimage.jpeg" alt=""width="30"\>
-                    <img id="1" alt="edit" onclick="update(this)"
+                    <img id="${empPayrollData._id}" alt="edit" onclick="update(this)"
                         src="images/editimages.jpeg" alt=""width="30"\>
                 </td>
             </tr>
-`;
+    `;
+}
 document.querySelector('#table-display').innerHTML = innerHtml;
 }
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
